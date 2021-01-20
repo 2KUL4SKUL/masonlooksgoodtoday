fd4bafb01cce44d22d9782f27d180925b5652c33
a.m.student <minorbrosadam@gmail.com>
2021-01-20 10:44:00 -0600

    Initial Commit

---
+++ Shared/masonislookinggoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  masonislookinggoodApp.swift
+//  Shared
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct masonislookinggoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
